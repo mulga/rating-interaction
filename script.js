dabc549eca225145c60d0bc710dbde1180df0ae9
Mehmet Ulga <mehmetulga@gmail.com>
2022-04-10 01:18:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,64 +8,98 @@ let rateResult = document.getElementById("rateGiven");
 let ratingContainer = document.getElementById("rating");
 let ratingThankYou = document.getElementById("thankYou");
 
-// function rating() {
-//   if (rateInput1) {
-//     result = "1";
-//     // rateInput1.style.backgroundColor = "red";
-//     rateResult.innerHTML = result;
-//     console.log("1");
-//   } else if (rateInput2) {
-//     result = "2";
-//     rateResult.innerHTML = result;
-//     console.log("2");
-//   }
-// }
+// rating
 
 function rating1() {
   result = "1";
-  // rateInput1.style.backgroundColor = "red";
+
   rateInput1.style.backgroundColor = "hsl(217, 12%, 63%)";
   rateInput1.style.color = "#fff";
   rateResult.innerHTML = result;
-  //   console.log("1");
+  //
+
+  rateInput2.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput2.style.color = "hsl(217, 12%, 63%)";
+  rateInput3.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput3.style.color = "hsl(217, 12%, 63%)";
+  rateInput4.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput4.style.color = "hsl(217, 12%, 63%)";
+  rateInput5.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput5.style.color = "hsl(217, 12%, 63%)";
+
+  //
 }
 
 function rating2() {
   result = "2";
-  // rateInput1.style.backgroundColor = "red";
+
   rateInput2.style.backgroundColor = "hsl(217, 12%, 63%)";
   rateInput2.style.color = "#fff";
   rateResult.innerHTML = result;
-  //   console.log("1");
+  //
+  rateInput1.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput1.style.color = "hsl(217, 12%, 63%)";
+  rateInput3.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput3.style.color = "hsl(217, 12%, 63%)";
+  rateInput4.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput4.style.color = "hsl(217, 12%, 63%)";
+  rateInput5.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput5.style.color = "hsl(217, 12%, 63%)";
 }
 function rating3() {
   result = "3";
-  // rateInput1.style.backgroundColor = "red";
+
   rateInput3.style.backgroundColor = "hsl(217, 12%, 63%)";
   rateInput3.style.color = "#fff";
   rateResult.innerHTML = result;
-  //   console.log("1");
+  //
+  rateInput1.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput1.style.color = "hsl(217, 12%, 63%)";
+  rateInput2.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput2.style.color = "hsl(217, 12%, 63%)";
+  rateInput4.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput4.style.color = "hsl(217, 12%, 63%)";
+  rateInput5.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput5.style.color = "hsl(217, 12%, 63%)";
 }
 
 function rating4() {
   result = "4";
-  // rateInput1.style.backgroundColor = "red";
+
   rateInput4.style.backgroundColor = "hsl(217, 12%, 63%)";
   rateInput4.style.color = "#fff";
   rateResult.innerHTML = result;
-  //   console.log("1");
+  //
+
+  rateInput1.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput1.style.color = "hsl(217, 12%, 63%)";
+  rateInput2.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput2.style.color = "hsl(217, 12%, 63%)";
+  rateInput3.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput3.style.color = "hsl(217, 12%, 63%)";
+
+  rateInput5.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput5.style.color = "hsl(217, 12%, 63%)";
 }
 
 function rating5() {
   result = "5";
-  // rateInput1.style.backgroundColor = "red";
+
   rateInput5.style.backgroundColor = "hsl(217, 12%, 63%)";
   rateInput5.style.color = "#fff";
   rateResult.innerHTML = result;
-  //   console.log("1");
+  //
+  rateInput1.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput1.style.color = "hsl(217, 12%, 63%)";
+  rateInput2.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput2.style.color = "hsl(217, 12%, 63%)";
+  rateInput3.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput3.style.color = "hsl(217, 12%, 63%)";
+  rateInput4.style.backgroundColor = "hsl(213, 19%, 18%)";
+  rateInput4.style.color = "hsl(217, 12%, 63%)";
 }
 
-// submit result
+// submit rating
 
 submitButton.addEventListener("click", function () {
   document.querySelector(".rating").style.display = "none";
